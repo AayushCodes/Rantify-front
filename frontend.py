4f590a32f6aea39b8886a5bda3af428f3db9a5ee
ParkhiSaini <parkhi.saini2401@gmail.com>
2021-12-29 23:05:02 +0530

    logo.png added

--- frontend.py
+++ frontend.py
@@ -1,16 +1,27 @@
 from tkinter import *
+import tkinter.font as font
+from PIL import ImageTk, Image  
+
+
 
 
 #creating a window
 window=Tk()
+window.option_add('*Font', 'Times')
 window.geometry('500x500')
 c1 = Canvas(window, width=500, height=300)
 window.title('Rantify')
-head=Label(window, text='RANTIFY').pack()
+
+#header
+
+head=Label(window, text='RANTIFY',).pack()
+subtext=Label(window,text="The music you like").pack()
+
 
 #toplevel PhotoImage
 p1 = PhotoImage(file = 'logo.png')
 window.iconphoto(False, p1)
+
 
 #logo image
 # img=Image.open('logo.png')
@@ -18,9 +29,18 @@ window.iconphoto(False, p1)
 # image=Label(window, image=img).grid(row=0, column=1)
 
 #login button
-logb = Button(window, text='Log in', width=25)
-logb.place(relx = 1.0, rely = 0.0,anchor='ne')
-logb.pack(pady = 10)
+logb = Button(window, text='Log in', width=10)
+logb.place(x=1100,y=11)
+
+#signin button 
+signinb = Button(window, text='Sign in', width=10)
+signinb.place(x=950,y=11)
+
+'''bg = PhotoImage(file = "background.png")
+backimg = Label( window, image = bg)
+backimg.pack()'''
+
+
 
 
   
