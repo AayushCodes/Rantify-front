0073dcc7b9c4340531ee1770f103b4dddc68d535
Yash Chowdhury <yash@Yashs-MacBook-Air.local>
2022-01-20 04:39:43 +0530

    Scaled up

--- landing_page.py
+++ landing_page.py
@@ -6,7 +6,6 @@ import tkinter.font
 #window 
 root=Tk()
 root.title('Rantify-Music That You Like')
-<<<<<<< HEAD
 root.iconbitmap('icon.ico')
 root.geometry('1920x1080')
 
@@ -15,10 +14,8 @@ label = Label(root, image = img)
 label.pack(side='right')
 
 
-=======
 root.iconbitmap('D:\Github\Rantify-front\icon.ico')
 root.geometry('500x450')
->>>>>>> 39fbe8b33c7c7715954bf93dd158d0f7594ad7ea
 
 #background of the landing page 
 bg=PhotoImage(file = "D:\Github\Rantify-front\Background_landingpage.png")
@@ -33,7 +30,6 @@ head=Label(root,text='Rantify',font=("Lovelo", 25, "bold"),bg='#97D5FA').pack()
 sub_head=Label(root,text='Music That You Like',font=("Roboto", 15, "italic"),bg='#97D5FA').pack()
 '''bg='#97D5FA'''
 
-<<<<<<< HEAD
 '''#logo resize
 pic=Image.open('"D:/Github\Rantify-front\logo.png"')
 resized=pic.resize((100,100),Image.ANTIALIAS)
@@ -44,12 +40,10 @@ newlogo=ImageTk.PhotoImage(resized)'''
 '''canvas = Canvas(root, width = 100, height = 100)
 canvas.pack()'''
 img = PhotoImage(file="log.png")
-=======
 #logo
 canvas = Canvas(root, width = 99, height = 99)
 canvas.place(x=0,y=0)
 img = PhotoImage(file="D:\Github\Rantify-front\log.png")
->>>>>>> 39fbe8b33c7c7715954bf93dd158d0f7594ad7ea
 Button(root,  image = img).place()
 canvas.create_image(1,1, anchor=NW, image=img)
 
--- ui_4.py
+++ ui_4.py
@@ -5,29 +5,27 @@ from PIL import ImageTk, Image
 
 win = Tk()
 win.title("UI-4")
-win.geometry('500x450')
+win.geometry('1920x1080')
 
-#logo = Image.open("logo.png")
-#logo_res = logo.resize(70, 70)
+'''logo = Image.open("logo.png")
+logo_res = logo.resize(70, 70)
+img = Label(win, image=logo).place(x=10, y=30)'''
 
- 
-#img = Label(win, image=logo).place(x=10, y=30)
-
-head1 = Label(win, text="Rantify", font=("Lovelo", 20, "bold")).pack()
-head2 = Label(win, text="The Music You Like", font=("Roboto", 15, "italic")).pack()
+head1 = Label(win, text="Rantify", font=("Lovelo", 40, "bold")).pack()
+head2 = Label(win, text="The Music You Like", font=("Roboto", 30, "italic")).pack()
 
 
-rt = Label(win, text="Title").place(x = 40, y = 70)
-e1 = Entry(win).place(x = 80, y = 70)
+rt = Label(win, text="Title", font=("", 30)).place(x = 200, y = 160)
+e1 = Entry(win).place(x = 270, y = 165, width=500, height=37)
 
-rant = Label(win, text="RANT:").place(x = 40, y = 100) 
+rant = Label(win, text="RANT:", font=("", 27)).place(x = 200, y = 220) 
 
 
-rant_text = ScrolledText(win, height = 10, width = 50)
-rant_text.place(x = 40, y = 130)
+rant_text = ScrolledText(win, height = 20, width = 100)
+rant_text.place(x = 205, y = 280)
 rant_text.insert("1.0", "Type Here...")
 
-but1 = Button(win, text="I Am Done!").place(x = 320, y = 280)
+but1 = Button(win, text="I Am Done!",font=("", 25)).place(x = 800, y = 570)
 
 
 win.mainloop()
