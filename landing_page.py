e81fab9f11ff6c3a442250e845086d8f764305d2
ParkhiSaini <parkhi.saini2401@gmail.com>
2022-01-19 23:12:17 +0530

    landing page and two question pages

--- landing_page.py
+++ landing_page.py
@@ -2,7 +2,6 @@ from tkinter import*
 from PIL import ImageTk, Image
 import tkinter.font
 
-
 #window 
 root=Tk()
 root.title('Rantify-Music That You Like')
@@ -10,30 +9,40 @@ root.iconbitmap('D:\Github\Rantify-front\icon.ico')
 root.geometry('500x450')
 
 #background of the landing page 
-bg=PhotoImage(file = "D:\Github\Rantify-front\Background_landingpage.png")
+'''bg=PhotoImage(file = "D:\Github\Rantify-front\gradnew.png")
 bg1=Label(root,image=bg)
-bg1.place(x=0,y=0,relwidth=1,relheight=1)
+bg1.place(x=0,y=0,relwidth=1,relheight=1)'''
+root.config(bg='#050A30')
 
 #header
-head=Label(root,text='Rantify',font=("Lovelo", 25, "bold"),bg='#97D5FA').pack()
+head=Label(root,text='Rantify',font=("Kollektif", 30),bg='#050A30',fg='white').place(x=75,y=3)
 '''bg='#97D5FA'''
 
 #sub-header
-sub_head=Label(root,text='Music That You Like',font=("Roboto", 15, "italic"),bg='#97D5FA').pack()
+'''sub_head=Label(root,text='Music That You Like',font=("Roboto", 15, "italic")).place(x=200,y=1)'''
 '''bg='#97D5FA'''
 
 #logo
-canvas = Canvas(root, width = 99, height = 99)
-canvas.place(x=0,y=0)
-img = PhotoImage(file="D:\Github\Rantify-front\log.png")
-Button(root,  image = img).place()
+canvas = Canvas(root, width = 39, height = 39)
+canvas.place(x=3,y=3)
+img = PhotoImage(file="D:\Github\Rantify-front\ewlog.png")
+Button(root,image = img).place()
 canvas.create_image(1,1, anchor=NW, image=img)
 
 #body 
-body=Label(root,text='Lorem ipsum dolor sit amet, consectetur adipiscing',font=('Helvetica','26'),bg='#B3E1FB').place(x=100,y=200)
-body1=Label(root,text='sed do eiusmod tempor incididunt ut labore et',font=('Helvetica','20'),bg='#B6E2FB').place(x=100,y=250)
-body2=Label(root,text='sed do eiusmod tempor incididunt ut labore et',font=('Helvetica','18'),bg='#BDE3FD').place(x=100,y=300)
-body2=Label(root,text='sed do eiusmod tempor incididunt ut labore et',font=('Helvetica','18'),bg='#C4E6FF').place(x=100,y=350)
+body=Label(root,text='Rant anytime without',font=('Sans-serif','32'),bg='#050A30',fg='white').place(x=100,y=150)
+body1=Label(root,text='feeling insecure',font=('Helvetica','32'),bg='#050A30',fg='white').place(x=100,y=200)
+body2=Label(root,text='sed do eiusmod tempor incididunt ut labore et',font=('Helvetica','18'),bg='#050A30',fg='white').place(x=100,y=300)
+body2=Label(root,text='sed do eiusmod tempor incididunt ut labore et',font=('Helvetica','18'),bg='#050A30',fg='white').place(x=100,y=350)
+
+#side graphic
+canvas1 = Canvas(root, width = 500, height = 500,bd=7,relief=RAISED)
+canvas1.place(x=700,y=90)
+img1 = PhotoImage(file="D:\Github\Rantify-front\sidegraphic.png")
+canvas1.create_image(0,0,anchor=NW, image=img1)
+
+'''graphic=PhotoImage(file="D:\Github\Rantify-front\graphic.png")
+img2=Label(root,image=graphic).pack(side="right")'''
 
 #buttons
 #login button
@@ -43,9 +52,6 @@ logb.place(x=100,y=500)
 #signin button 
 signinb = Button(root, text='Sign in',font=('Helvetica',20,'bold') ,width=10,bg='white' )
 signinb.place(x=350,y=500)
-
-
-
 
 
 root.mainloop()
